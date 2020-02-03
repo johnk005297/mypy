d239ea1711f5a462e9fb954a1555c69cd8030a6f
Ivan Kutuzov <kutuzov@mmsi.com>
2020-02-03 11:39:15 +0300

    first commit

---
+++ m.py
@@ -0,0 +1,13 @@
+
+    
+while True:
+    x = int(input("Give me the number: "))
+    if x > 100:
+        print("Print lower number")
+    elif x <= 50:
+        print("Try more")
+    else:
+        print("No good this time!")
+        break
+
+
