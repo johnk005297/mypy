992f25a12824f79715e618a83b5b59ef68132a5a
rupert_at_work <kutuzov@mmsi.com>
2020-02-03 17:59:13 +0300

    commit with new user.name+email

--- m.py
+++ m.py
@@ -1,19 +1,2 @@
-def ask_user(prompt, retries=3, hint="Ответьте, ДА или НЕТ?"):
-    while True:
-        retries -= 1
-        ok = input(prompt + " -> ").upper()
-
-        if ok in ("Д", "ДА"):
-            return True
-        elif ok in ("Н", "НЕТ"):
-            return False
-
-        if retries <= 0:
-            print("Не смог получить нужный ответ, считаю за отказ.")
-            return False
-        print(hint)
-
-if ask_user("Сохранить файл?"):
-    print("Сохранил!")
-else:
-    print("Не сохранил.")
+def test(*args, **kwargs):
+    pass
