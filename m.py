b96537f2f90c5b0c74d98ef19d11c00cb60ad882
rupert_at_work <kutuzov@mmsi.com>
2020-02-14 13:28:09 +0300

    return to func

--- m.py
+++ m.py
@@ -2,13 +2,13 @@ def test(a, b, c, units="сантиметры", print_error=True):
     if a + b <= c or a + c <= b or b + c <= a:
         if print_error:
             return "Проверьте введенные стороны треугольника!"
-        return
+        
 
     p = (a + b + c) / 2
     s = (p * (p - a) * (p - b) * (p - c)) ** 0.5  
     return "{} {}".format(round(s,2), units)
 
-abc = [6, 4, 5]
+abc = [6, 4, 3]
 params = dict(units="см.",print_error=True)
 
 print(test(*abc, **params))
