2ee9377d34ce7d00cdf3e5a657841b7eceb42125
rupert_at_work <kutuzov@mmsi.com>
2020-03-27 02:14:34 +0300

    smth with new line

--- m.py
+++ m.py
@@ -1,17 +1,16 @@
-class tclass:
+class myMath:
     def __init__(self):
         try:                
             self.x = float(input("enter first num: "))
             self.y = float(input("enter sec num: "))
         except Exception as err:
-            print("Not float, error: {}".format(err))
+            print("Not float, error: {}".format(err))            
             raise
 
-    # def __str__(self):
-    #     return "{} and {}".format(self.x, self.y)
+    
 
     def summingUp(self):
-                  
+               
         if self.x == self.y:
             return "They are even: {} = {}".format(self.x, self.y)
 
@@ -20,12 +19,15 @@ class tclass:
         # else:
         #     raise TypeError("Not a number: {} or {}".format(type(self.x), type(self.y)))
         
-    
+    def __str__(self):
+        return "\nYour numbers are: {} and {}".format(self.x, self.y)
 
 if __name__ == "__main__":
 
-    c = tclass()    
+    c = myMath()    
+    print(c)
     print(c.summingUp())
+    
     
     
     
