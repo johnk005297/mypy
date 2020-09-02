7386e5a5ae8a702ab9e879ac8f9fe9fd7d102f0b
rupert_at_work <kutuzov@mmsi.com>
2020-09-02 18:39:12 +0300

    After long break

--- m.py
+++ m.py
@@ -1,32 +1,36 @@
 class myMath:
-    def __init__(self):
-        try:                
-            self.x = float(input("enter first num: "))
-            self.y = float(input("enter sec num: "))
-        except Exception as err:
-            print("Not float, error: {}".format(err))            
-            raise
+    def __init__(self):                      
+        self.a = 155
+        self.b = 962    
+        self.d = "Description of the instance"
 
-    
-
-    def summingUp(self):
-               
-        if self.x == self.y:
-            return "They are even: {} = {}".format(self.x, self.y)
-
-        else:
-            return "The result off summing: {}".format(self.x + self.y)
-        # else:
-        #     raise TypeError("Not a number: {} or {}".format(type(self.x), type(self.y)))
+    def summingUp(self, x, y):
         
-    def __str__(self):
-        return "\nYour numbers are: {} and {}".format(self.x, self.y)
+        try:
+            if x == y:
+                return "They are even: {} = {}".format(x, y)
+
+            else:
+                return "The result off summing: {}".format(x + y)
+        except Exception as err:
+            print("There is an error!")
+            print("Type: ", type(err))
+            print(err)
+        
+    # def __str__(self):
+    #     return "\nYour object are: {0}({1}) and {2}({3}) \n{4}".format(self.x,type(self.x), self.y, type(self.y), self.d)
+    # def __str__(self):
+    #     return "{}. {} {} ".format(self.d, self.a, self.b)
+        
 
 if __name__ == "__main__":
-
+       
     c = myMath()    
-    print(c)
-    print(c.summingUp())
+    print(c)    
+    print(c.summingUp(27.60,13.74))
+    print(myMath.summingUp(c,5,7))
+
+    
     
     
     
