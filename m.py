0909e9ed6fb694ae46550815dcd3c20dd63c7dfe
rupert_at_work <kutuzov@mmsi.com>
2021-06-09 14:55:26 +0300

    Checking git bash cmd

--- m.py
+++ m.py
@@ -24,7 +24,7 @@ print("# Соц. карта:", "Да" if has_discount else "Нет")
 print("# Скидка: {}", )
 print("# Итоговая цена: {}", final_price)
 # if we would like to change smth !!!
+# checking from bash cmd
 
     
 #   git commit <file_name> -m "<Message text>"
-# git setup 
