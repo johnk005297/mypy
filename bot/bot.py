c62035bf3a7a977f5e701e7f622d1b967f37b360
johnk005297 <johnk005297@gmail.com>
2021-09-30 21:21:52 +0300

    conceal bot api key

--- bot/bot.py
+++ bot/bot.py
@@ -1,7 +1,7 @@
 from telegram.ext import Updater
 
 def main():
-    mybot = Updater("2021736250:AAEYroXQ5sbWIwaj0HukMVhwy6C7cNNdp04",use_context=True)
+    mybot = Updater("api_key",use_context=True)
 
     mybot.start_polling()
     mybot.idle()
