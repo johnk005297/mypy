278e26f79e066cbc96549671af946e57ded1bc09
johnk005297 <johnk005297@gmail.com>
2021-09-30 20:58:06 +0300

    save bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -1,1 +1,9 @@
-ECHO is on.
+from telegram.ext import Updater
+
+def main():
+    mybot = Updater("2021736250:AAEYroXQ5sbWIwaj0HukMVhwy6C7cNNdp04",use_context=True)
+
+    mybot.start_polling()
+    mybot.idle()
+
+main()
