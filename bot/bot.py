8b66c6e16b97db15af7d2c3a8634d81898b05f02
johnk005297 <johnk005297@gmail.com>
2021-10-03 20:32:09 +0300

    adding a few functions for bot

--- bot/bot.py
+++ bot/bot.py
@@ -1,11 +1,15 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 # MessageHandler - это широкий обработчик, который может реагировать на большой тип сообщений.
 # CommandHendler - это узко-нишевой обработчик, который реагирует только на команды.
-import logging
+import logging                      # used in take_logs function
 import settings
+from random import randint          # used in play_random_number function
+from glob import glob               # used in send_picture function
 
 
-logging.basicConfig(filename="bot.log", level=logging.INFO)
+def take_logs():    
+    logging.basicConfig(filename="bot.log", format="%(asctime)s  %(levelname)s:%(name)s  %(message)s", level=logging.INFO, datefmt="%Y-%b-%d %H:%M:%S") 
+        
 
 def greet_user(update, context):        # every function must have two args: update and context
     update.message.reply_text("Hello, user!")
@@ -14,19 +18,31 @@ def talk_to_me(update,context):
     text = update.message.text      # input from the user    
     update.message.reply_text(text) # bots reply message in telegram
 
-def guess_number(update, context):
-    print(context.args)
+def play_random_number(user_number):
+    bot_number = randint(user_number - 10, user_number + 10)    
+    if user_number > bot_number:
+        message = f"Your number {user_number}, mine {bot_number}. You win!"
+    elif user_number == bot_number:
+        message = f"Your number {user_number}, mine {bot_number}. It's a draw!"
+    elif user_number < bot_number:
+        message = f"Your number {user_number}, mine {bot_number}. I win! Hehehe"
+    else: pass
+    return message
+
+def guess_number(update, context):    
     if context.args:
         try:
-            user_number = int(context.args[0])
-            message = f"You number is {user_number}"
+            user_number = int(context.args[0])  # context.args is a list, so taking 1st element by index
+            # message = f"You number is {user_number}"
+            message = play_random_number(user_number)
         except(TypeError, ValueError):
             message = "Need to enter integer!"
     else:
         message = "Enter a number!"    
     update.message.reply_text(message)
 
-
+def send_picture():
+    pass
 
 def main():
     mybot = Updater(settings.API_KEY,use_context=True)
@@ -34,12 +50,15 @@ def main():
     disp = mybot.dispatcher
     disp.add_handler(CommandHandler("start", greet_user))
     disp.add_handler(CommandHandler("guess", guess_number))
-    disp.add_handler(MessageHandler(Filters.text, talk_to_me))
-    logging.info("Bot has started...")
+    disp.add_handler(CommandHandler("send_pic", send_picture))
+    disp.add_handler(MessageHandler(Filters.text, talk_to_me))    
     
     mybot.start_polling()
     mybot.idle()
 
 
-if __name__ == "__main__":
+if __name__ == "__main__":    
+    take_logs()
     main()
+    
+    
