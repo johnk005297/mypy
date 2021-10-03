02bb7bf763626134e0e08849b591b833b2f253de
johnk005297 <johnk005297@gmail.com>
2021-10-03 05:03:45 +0300

    Added input data from user

--- bot/bot.py
+++ bot/bot.py
@@ -7,18 +7,33 @@ import settings
 
 logging.basicConfig(filename="bot.log", level=logging.INFO)
 
-def greet_user(update, context):    
+def greet_user(update, context):        # every function must have two args: update and context
     update.message.reply_text("Hello, user!")
 
-def talk_to_me(update,context):
-    text = update.message.text    
-    update.message.reply_text(text)
+def talk_to_me(update,context):    
+    text = update.message.text      # input from the user    
+    update.message.reply_text(text) # bots reply message in telegram
+
+def guess_number(update, context):
+    print(context.args)
+    if context.args:
+        try:
+            user_number = int(context.args[0])
+            message = f"You number is {user_number}"
+        except(TypeError, ValueError):
+            message = "Need to enter integer!"
+    else:
+        message = "Enter a number!"    
+    update.message.reply_text(message)
+
+
 
 def main():
     mybot = Updater(settings.API_KEY,use_context=True)
 
     disp = mybot.dispatcher
     disp.add_handler(CommandHandler("start", greet_user))
+    disp.add_handler(CommandHandler("guess", guess_number))
     disp.add_handler(MessageHandler(Filters.text, talk_to_me))
     logging.info("Bot has started...")
     
