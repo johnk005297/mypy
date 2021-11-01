b009dfceb8b2b66c61596a9c122d4cab6cb97857
johnk005297 <johnk005297@gmail.com>
2021-11-01 16:46:26 +0300

    4.2.3 done

--- py_course/ex_4.2.3_.py
+++ py_course/ex_4.2.3_.py
@@ -1,1 +1,26 @@
+###
+week_days = ["Понедельник","Вторник","Среда","Четверг","Пятница","Суббота","Воскресенье"]
+x = int(input()) - 1
+
+if x%7 == 0:
+    print(week_days[x%7])
+elif x%7 == 1:
+    print(week_days[x%7])
+elif x%7 == 2:
+    print(week_days[x%7])
+elif x%7 == 3:
+    print(week_days[x%7])
+elif x%7 == 4:
+    print(week_days[x%7])
+elif x%7 == 5:
+    print(week_days[x%7])
+else:
+    print(week_days[-1])    
+
+
+### second variant
+
+# week_days = ["Понедельник","Вторник","Среда","Четверг","Пятница","Суббота","Воскресенье"]
+# x = int(input()) - 1
+# print(week_days[x%7])
 
