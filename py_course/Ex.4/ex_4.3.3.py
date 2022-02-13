696466ca5638e9d874ce1e9fe0c08aa4850a4697
johnk005297 <johnk005297@gmail.com>
2022-02-14 01:22:35 +0300

    one more change in 4.3.3

--- py_course/Ex.4/ex_4.3.3.py
+++ py_course/Ex.4/ex_4.3.3.py
@@ -4,12 +4,17 @@ lst: list = [1,2,4,8,16,32,64]
 n = int(input())
 a: list = []
 
-while True:
-    if sum(a) >= n:
-        print(*a)
-        break
-    for x in lst[::-1]:
-        while x <= (n - sum(a)):            
-            a.append(x)
+# while True:
+#     if sum(a) >= n:
+#         print(*a)
+#         break
+#     for x in lst[::-1]:
+#         while x <= (n - sum(a)):            
+#             a.append(x)
             
-        
+
+#   ****** Shorter than above ******       
+for x in lst[::-1]:
+    while x <= (n - sum(a)):
+        a.append(x)
+print(*a)
