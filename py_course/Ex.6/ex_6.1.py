7f3705d99486358f9cb6df007ad139f3c1d4958f
johnk005297 <johnk005297@gmail.com>
2022-05-19 11:40:25 +0300

    creating api test

--- py_course/Ex.6/ex_6.1.py
+++ py_course/Ex.6/ex_6.1.py
@@ -1,20 +1,5 @@
 #
-import json
-import requests
-from dotenv import load_dotenv
-import os
 
-load_dotenv()
-url = "http://std-h5.dev.bimeister.io/api/Geometries/Statistics/Similar"
-
-# headers = {'Accept': 'application/json'}
-token = os.getenv("token")
-headers = {'Authorization': f'Bearer {token}'}
-
-
-response = requests.get(url, headers=headers)
-
-print(response.json())
 
 
 
