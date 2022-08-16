652562053d5b54999431677954da09f6537c0065
johnk005297 <johnk005297@gmail.com>
2022-08-16 17:43:52 +0300

    added check privilege function to update license script

--- bim_project/license_activation/update_license.py
+++ bim_project/license_activation/update_license.py
@@ -33,22 +33,24 @@ def define_purpose():
     else:
         sys.exit("Exit.")
 
-def get_data_from_license_file():
-
-    data_from_lic_file: dict = {}
+def get_license_token():
 
     # Check if the *.lic file is in place
     if os.path.isfile(f"{pwd}/license.lic"):
 
         with open("license.lic", "r", encoding="utf-8") as file:    # get license_token from the .lic file and put it into data_from_lic_file dictionary
-            data_from_lic_file['license_token'] = file.read().split()[0].strip("\"")
+            license_token = file.read().split()[0].strip("\"")
             
-        return data_from_lic_file
+        return license_token
     
     else:
-        logging.error("No license.lic file in the folder. Exit.")
-        sys.exit("No license.lic file in the folder. Exit.")
-
+        # logging.error("No license.lic file in the folder. Exit.")
+        # sys.exit("No license.lic file in the folder. Exit.")
+        license_token = input("There is no license.lic file in the folder.\nEnter license token manually or 'q' for exit: ")
+        if license_token == 'q':
+            logging.error("No license token has been provided by the user.")
+            sys.exit()
+        return license_token
 
 def creds():
 
@@ -111,12 +113,142 @@ def get_token():
     return token
 
 
+'''   Check user privileges   '''
+def check_user_privileges():
+    
+    '''  Users aren't authorized to create or edit their own system roles. Thus, need to check for {'name': 'Licenses', 'operation': 'Write'} privileges in system role.
+         If user doesn't have it, create another user, so we could be able to grant all privileges in system role for our current user.         
+    '''
+    url_users = f"{data_for_connect['url']}/api/Users"
+    def check_user():        
+        '''   Check if the user has needed permissions in his system roles already. # {'name': 'Licenses', 'operation': 'Write'}   '''
+
+        # user_names_and_system_roles_id: dict = {}    # dictionary to collect user's permissions.
+        user_system_roles_id: list = []         # list to collect user's permissions
+
+        headers_get_users = {'Content-type':'text/plane', 'Authorization': f"Bearer {token}"}
+        request = requests.get(url_users, headers=headers_get_users, verify=False)
+        response = request.json()
+        
+        for x in range(len(response)):
+            if data_for_connect['username'] == response[x].get('userName'):
+                for role in response[x]['systemRoles']:
+                    user_system_roles_id.append(role.get('id'))
+
+                    ''' We're taking userName and fill our dictionary with it. In format {'userName': [systemRoles_id1, systemRoles_id2, ...}
+                        Two options to create a dictionary we need. '''
+                    # user_names_and_system_roles_id.setdefault(data_for_connect['username'], []).append(role.get('id'))     # Option one                                                                                                            
+                    # user_names_and_system_roles_id[data_for_connect['username']] = user_names_and_system_roles_id.get(data_for_connect['username'], []) + [role.get('id')]    # Option two
+
+        for id in user_system_roles_id:
+            url_check_system_role = f"{data_for_connect['url']}/api/SystemRoles/{id}"
+            try:
+                request = requests.get(url_check_system_role, headers=headers_get_users, verify=False)
+                response = request.json()
+            except possible_request_errors as err:
+                logging.error(err)
+
+            for dct in response.get('permissions', False):
+                if dct.get('name') == 'Licenses' and dct.get('operation') == 'Write':
+                    return True
+
+        return False           
+
+    
+    if not check_user():
+        
+        '''   If the user doesn't have required permissions -> create another user and assing him a system role.   '''
+        
+        headers_create_user_and_system_role = {'accept': '*/*', 'Content-type': 'application/json-patch+json', 'Authorization': f"Bearer {token}"}    
+        payload_create_user = {
+                    "firstName": "Johnny",
+                    "lastName": "Mnemonic",
+                    "middleName": "superuser",
+                    "userName": "00000137438691328",  # the seventh number in the series of perfect numbers  137438691328  2305843008139952128
+                    "displayName": "Johnny_Mnemonic",
+                    "password": "Qwerty12345!",
+                    "phoneNumber": "+71234567890",
+                    "email": "the_matrix@exists.neo",
+                    "birthday": "2000-08-16T07:15:07.731Z",
+                    "position": "The_one"
+                }
+        data = json.dumps(payload_create_user)
+        try:
+            request = requests.post(url_users, headers=headers_create_user_and_system_role, data=data, verify=False)
+            response = request.json()
+            if request.status_code == 201:
+                logging.debug(f"'{response['userName']}' user was created successfully.")
+                created_user_name: str = response['userName']
+                created_user_id: str = response['id']
+            # in case if userName already exists, try another userName. Which is the eighth number in the series of perfect numbers by the way.
+            elif request.status_code == 409:
+                altered_data = data.replace('137438691328', '2305843008139952128')
+                request = requests.post(url_users, headers=headers_create_user_and_system_role, data=altered_data, verify=False)
+                if request.status_code == 201:
+                    logging.debug(f"'{request.text}' user was created successfully.")
+                    created_user_name: str = response['userName']
+                    created_user_id: str = response['id']
+                else:
+                    logging.error(request.text)
+        except possible_request_errors as err:            
+            logging.error(f"{err}\n{request.text}")
+
+        # Create a system role for a new user
+        payload_create_system_role = {
+                            "name": created_user_name,
+                            "permissions": [
+                                                {'name': 'CreateProject', 'operation': 'Write'},
+                                                {'name': 'ObjectModels', 'operation': 'Write'},
+                                                {'name': 'Roles', 'operation': 'Write'},
+                                                {'name': 'Security', 'operation': 'Write'},
+                                                {'name': 'Licenses', 'operation': 'Write'},
+                                                {'name': 'Processes', 'operation': 'Write'},
+                                                {'name': 'Ldap', 'operation': 'Write'},
+                                                {'name': 'Groups', 'operation': 'Write'},
+                                                {'name': 'MailServer', 'operation': 'Write'},
+                                                {'name': 'Users', 'operation': 'Write'},
+                                                {'name': 'Logs', 'operation': 'Read'}
+                                            ]
+                            }
+
+        url_create_system_role = f"{data_for_connect['url']}/api/SystemRoles"
+        data = json.dumps(payload_create_system_role)
+        try:
+            request = requests.post(url_create_system_role, headers=headers_create_user_and_system_role, data=data, verify=False)
+            response = request.json()
+            if request.status_code == 201:
+                logging.debug("System role was created successfully.")
+                created_system_role_id: str = response
+            else:
+                logging.error(request.text)
+        except possible_request_errors as err:
+            logging.error(f"{err}\n{request.text}")
+
+        # add system role to a new user /api/Users/AddSystemRole
+        print()
+        url_add_system_role_to_user = f"{data_for_connect['url']}/api/Users/AddSystemRole"
+        payload_add_system_role = {
+                                    "systemRoleId": created_system_role_id,
+                                    "userId": created_user_id
+                                }
+        data = json.dumps(payload_add_system_role)
+        try:
+            request = requests.post(url_add_system_role_to_user, headers=headers_create_user_and_system_role, data=data, verify=False)
+            if request.status_code in (201, 204):
+                logging.debug(f"System role '{response}' to user '{created_user_name}' added successfully.")
+            else:
+                logging.error(request.text)
+        except possible_request_errors as err:
+            logging.error(f"{err}\n{request.text}")
+
+
+
 
 '''   Show all the licenses in the system   '''
 def show_licenses():
     print()
     url = f"{data_for_connect['url']}/api/License"
-    headers = {'Content-type':'text/plane', 'Authorization': f"Bearer {token}"}
+    headers = {'accept': '*/*', 'Content-type':'text/plane', 'Authorization': f"Bearer {token}"}
     payload = {
                 "username": data_for_connect['username'],
                 "password": data_for_connect['password']
@@ -215,10 +347,8 @@ def post_license():
 
     headers = {'accept': 'text/plane', 'Content-Type': 'application/json-patch+json', 'Authorization': f"Bearer {token}"}
     url = f"{data_for_connect['url']}/api/License/"
-
-    license_token = get_data_from_license_file()['license_token']
     
-    data = json.dumps(license_token)
+    data = json.dumps(get_license_token())
 
     request = requests.post(url, headers=headers, data=data, verify=False)
     for license in get_license():
@@ -258,6 +388,7 @@ def put_license():
 if __name__ == "__main__":
     data_for_connect = creds()
     token = get_token()
+    check_user_privileges()   # Function isn't finished yet.
     while True:
         goal = define_purpose()
         if goal == 'update':
